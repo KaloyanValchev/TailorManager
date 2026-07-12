ee576ca8cd7ce78e2f24ca30e2740f6186e16e1e
KaloyanValchev <valchev3@gmail.com>
2026-07-12 18:55:25 +0300

    Initial commit

---
+++ tailorshop/src/test/java/com/kaloyan/tailorshop/TailorshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaloyan.tailorshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailorshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
